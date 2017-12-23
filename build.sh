1c6392dd61b284496491a09caf75268f1a544589
Sam Herrmann <samherrmann.github@gmail.com>
2017-12-22 21:48:51 -0500

    Build image as latest and tag with version

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ NG_CLI_VERSION=1.6.2
 
 docker build \
   --build-arg NG_CLI_VERSION=$NG_CLI_VERSION \
-  -t samherrmann/angular-cli:$NG_CLI_VERSION .
+  -t samherrmann/angular-cli .
+
+docker tag samherrmann/angular-cli samherrmann/angular-cli:$NG_CLI_VERSION
