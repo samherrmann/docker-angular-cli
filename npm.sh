904e7cfbc4bb4d14f2f70fae11f027326f404b7a
Sam Herrmann <samherrmann.github@gmail.com>
2018-04-06 16:58:20 -0400

    Add shebang in npm.sh script

--- npm.sh
+++ npm.sh
@@ -1,4 +1,5 @@
-# This is a sample script for using the container to run npm commands.
+#!/usr/bin/env bash
+# This is a sample script for using the container.
 
 args=()
 [[ $@ == 'start'* ]] && args+=('--publish 4200:4200')
