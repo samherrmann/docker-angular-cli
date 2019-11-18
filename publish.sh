a48be79244ee5d118219f3fae3ff534fbac91349
Sam Herrmann <samherrmann.github@gmail.com>
2019-11-18 10:17:54 -0500

    Publish versioned and latest tags

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source env.sh
-docker push samherrmann/angular-cli:$NG_CLI_VERSION
+docker push samherrmann/angular-cli:$NG_CLI_VERSION && docker push samherrmann/angular-cli:latest
